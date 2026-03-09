24d2d6d9587ae6bbf8d7347be7475f230a25daef
michaelkibet123 <michaelkibet123@gmail.com>
2026-03-09 17:00:41 +0300

    Fixed: Corrected numpy syntax error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import tensorflow as tf
 import tensorflow_hub as hub
 from PIL import Image
-import numpy as 
+import numpy as np 
 import requests
 from bs4 import BeautifulSoup
 import requests
