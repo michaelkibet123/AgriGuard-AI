9070290390344e02cf95068c5d145640e747a0bf
michaelkibet123 <michaelkibet123@gmail.com>
2026-04-12 19:37:33 +0300

    fix duplicate compile argument in model load

--- app.py
+++ app.py
@@ -331,7 +331,7 @@ SEV_COLORS = {
 @st.cache_resource
 def load_cassava_model():
     path = "agri_guard_brain_v2.keras"
-    return tf.keras.models.load_model(path, compile=False, custom_objects={}, compile=False)
+    return tf.keras.models.load_model(path, compile=False, custom_objects={})
 
 import os
 st.write(os.path.exists("agri_guard_brain_v2.keras"))
