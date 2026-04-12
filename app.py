c33015c99138b315f2e4ecfa43ab571753e8734a
michaelkibet123 <michaelkibet123@gmail.com>
2026-04-12 19:35:09 +0300

    add model existence debug check

--- app.py
+++ app.py
@@ -333,6 +333,8 @@ def load_cassava_model():
     path = "agri_guard_brain_v2.keras"
     return tf.keras.models.load_model(path, compile=False)
 
+import os
+st.write(os.path.exists("agri_guard_brain_v2.keras"))
 cassava_model = load_cassava_model()
 brain_model   = load_brain_model()
 
