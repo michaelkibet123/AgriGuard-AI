f902153c7df8f990c0f8754ab3ab510b55f7f88e
michaelkibet123 <michaelkibet123@gmail.com>
2026-04-12 12:47:55 +0300

    add base streamlit UI

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ import tensorflow as tf
 def load_cassava_model():
     path = "agri_guard_brain_v2.keras"
     return tf.keras.models.load_model(path, compile=False)
+
+st.title('AgriGuard AI')
+st.write('App is running successfully')
