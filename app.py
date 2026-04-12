6813eeece1673bdd2fec3b52d47c09f813aff4a8
michaelkibet123 <michaelkibet123@gmail.com>
2026-04-12 19:39:32 +0300

    final model load fix

--- app.py
+++ app.py
@@ -331,7 +331,7 @@ SEV_COLORS = {
 @st.cache_resource
 def load_cassava_model():
     path = "agri_guard_brain_v2.keras"
-    return tf.keras.models.load_model(path, compile=False, custom_objects={})
+    return tf.keras.models.load_model(path, compile=False, custom_objects={}, safe_mode=False)
 
 import os
 st.write(os.path.exists("agri_guard_brain_v2.keras"))
