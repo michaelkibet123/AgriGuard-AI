093de169259f3414a3397571c5f9bb79a5a1edcf
michaelkibet123 <michaelkibet123@gmail.com>
2026-04-12 12:53:17 +0300

    hard reset cassava model loader clean state

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import streamlit as st
 import tensorflow as tf
 
 
 
+
+st.title('AgriGuard AI')
+st.write('App is running successfully')
+
+
+
+
+@st.cache_resource
 def load_cassava_model():
     path = "agri_guard_brain_v2.keras"
     return tf.keras.models.load_model(path, compile=False)
 
-st.title('AgriGuard AI')
-st.write('App is running successfully')
+cassava_model = load_cassava_model()
